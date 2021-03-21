8a906736f08788482f19c73a2b9825f10609a497
uwasejeannine <jeannineuwasee@gmail.com>
2021-03-21 15:03:41 +0300

    Doneri

---
+++ inde.js
@@ -0,0 +1,22 @@
+
+function  getGrades(){
+    var marks=50;
+if(marks>=80)
+{
+console.log("A");
+}
+else if(marks>=70)
+{
+console.log("B");
+}
+else if(marks>=60){
+    console.log("c");
+}
+else if(marks>=50){
+    console.log("D");
+}
+else if(marks>=0){
+    console.log("E");
+}
+}
+getGrades();
